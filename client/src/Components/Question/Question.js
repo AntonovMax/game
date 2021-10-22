887c09d0924150b78b664576a9a5a578941bbd2d
Max <antonov.max.v@yandex.ru>
2021-10-22 17:37:30 +0300

    1737

--- client/src/Components/Question/Question.js
+++ client/src/Components/Question/Question.js
@@ -1,10 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import ModalWindow from '../ModalWindow/ModalWindow';
+
 
 function Question({ question }) {
 
+  const [openModal, setOpenModal] = useState(false)
   return (
-    <td key={question.id} style={{ cursor: 'pointer' }} id={question.id}>200</td>
+    <>
+    <td onClick={() => setOpenModal(true)} key={question.id} style={{ cursor: 'pointer' }} id={question.id}>{question.price}</td>
+    {
+      openModal && <ModalWindow question={question} openModal={openModal} setOpenModal={setOpenModal}/>
+    }
+    </>
   );
 }
 
