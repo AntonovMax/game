ad1d518798bfb57ec4e96fe173d7002c788434e0
Max <antonov.max.v@yandex.ru>
2021-10-22 17:52:05 +0300

    6666

--- client/src/Components/ModalWindow/ModalWindow.js
+++ client/src/Components/ModalWindow/ModalWindow.js
@@ -5,29 +5,34 @@ import xrest from './xrest.svg'
 
 function ModalWindow({ question, openModal, setOpenModal }) {
 
-  const [result, setResult] = useState(false);
+  const [result, setResult] = useState(null);
 
   function handlePopup(event) {
     event.preventDefault();
 
     const userText = event.target.modal.value;
+    console.log(question.answer)
+
+    console.log(userText)
 
     if (userText.toLowerCase() === question.answer.toLowerCase()) {
-      setResult(true);
+      setResult('yes');
+    } else {
+      setResult('no')
     }
   }
 
   return (
     <div className='modal_container'>
       <form onSubmit={handlePopup}>
-        <img onClick={() => setOpenModal(openModal)} style={{width: 30, height: 30}} src={xrest} alt="xrest" />
+        <img onClick={() => setOpenModal(false)} style={{width: 30, height: 30}} src={xrest} alt="xrest" />
         <p className="user_text">{question.question}</p>
         <div className="mb-3">
           <input type="text" className="form-control" name='modal' placeholder='Enter your answer' />
         </div>
         <button type="submit" className="btn btn-primary own_button">Submit</button>
         {
-          result ? <p>Правильно</p> : ''
+          result === 'yes' ? <p>Правильно</p> : result === 'no' ? <p>Неправильно: {question.answer}</p> : ''
         }
       </form>
     </div>
