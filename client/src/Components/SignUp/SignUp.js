ecbb39d4bdcbc5f54907a8267e1ae94a089ff31f
Arsen <arsen.dzharullaev26@gmail.com>
2021-10-22 14:30:02 +0300

    refact log reg

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -1,10 +1,14 @@
-import React from 'react';
+
 
 function SignUp(props) {
+
+  // const [name, setName] = useState('edd')
+  // const [pass, setPass] = useState('ssx')
 
   const handlerSubmit = (event) => {
     event.preventDefault()
 
+  
     fetch('localhost:4000/registration', {
       method: 'post',
       headers: {
@@ -19,15 +23,16 @@ function SignUp(props) {
     })
   }
 
+  
   return (
     <div className='block_container'>
       <form onSubmit={handlerSubmit}>
-        <div class="mb-3">
-          <input type="email" className="form-control" id="nameInput" name='name' aria-describedby="emailHelp" placeholder='Enter your name' />
+        <div className="mb-3">
+          <input type="text"   className="form-control" id="nameInput" name='name' aria-describedby="emailHelp" placeholder='Enter your name' />
           <div id="emailHelp" className ="form-text">Your name must be uniq</div>
         </div>
-        <div class="mb-3">
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Enter your password'/>
+        <div className="mb-3">
+          <input type="password"  name="password" className="form-control" id="exampleInputPassword1" placeholder='Enter your password'/>
         </div>
         <button type="submit" className="btn btn-primary own_button">Submit</button>
       </form>
